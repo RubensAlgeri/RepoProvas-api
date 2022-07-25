d48746e4ed5d65e51fae9a3aa8e07b2eae4a4a9a
Rubens Algeri <rubens_zys@hotmail.com>
2022-07-25 16:02:37 -0300

    style: divided app.test file into two files

--- tests/test.test.ts
+++ tests/test.test.ts
@@ -1,7 +1,5 @@
 import app from '../app.js';
 import supertest from 'supertest';
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import * as authFactory from './factories/authFactory.js';
 import * as testFactory from './factories/testFactory.js';
 import prisma from '../config/database.js';
@@ -14,81 +12,6 @@ beforeEach(async () => {
 
 afterAll(async () => {
     await prisma.$disconnect();
-});
-
-describe("Auth router tests", () => {
-
-    it("creating a user it should return 201", async () => {
-        const body = await authFactory.generateUser();
-        await supertest(app).post("/sign-up").send(body);
-
-        const user = await prisma.user.findUnique({where:{email:body.email}})
-        
-        expect(bcrypt.compareSync(body.password, user.password)).toEqual(true);
-    });
-
-    it("creating a user with an used email it should return 409", async () => {
-        const body = await authFactory.generateUser();
-        await supertest(app).post("/sign-up").send(body);
-        await supertest(app).post("/sign-up").send(body);
-
-        const user = await prisma.user.findMany({where:{email:body.email}})
-
-        expect(user.length).toEqual(1);
-    });
-
-    it("creating a user without an email it should return 422", async () => {
-        const body = await authFactory.generateUser();
-        delete body.email;
-        await supertest(app).post("/sign-up").send(body);
-        const user = await prisma.user.findMany({where:{email:body.email}})
-
-        expect(user.length).toEqual(0);
-    });
-
-    it("creating a user without an password it should return 422", async () => {
-        const body = await authFactory.generateUser();
-        delete body.password;
-        await supertest(app).post("/sign-up").send(body);
-        await supertest(app).post("/sign-up").send(body);
-        const user = await prisma.user.findMany({where:{email:body.email}})
-
-        expect(user.length).toEqual(0);
-    });
-
-    it("make login with a registered a user it should return a token", async () => {
-        const body = await authFactory.generateUser();
-        await supertest(app).post("/sign-up").send(body);
-        delete body.confirmedPassword;
-        const token = jwt.sign(body.email, process.env.JWT_SECRET);
-        const response = await supertest(app).post("/sign-in").send(body);
-
-        expect(response.text).toBe(token);
-    });
-
-    it("make login with a wrong password it should return a 401", async () => {
-        const body = await authFactory.generateUser();
-        await supertest(app).post("/sign-up").send(body);
-
-        delete body.confirmedPassword;
-
-        const body2 = await authFactory.generateUser();
-        body.password=body2.password;
-
-        const token = jwt.sign(body.email, process.env.JWT_SECRET);
-        const response = await supertest(app).post("/sign-in").send(body);
-
-        expect(response.text).not.toBe(token);
-    });
-
-    it("make login with a unregistered email it should return a 404", async () => {
-        const body = await authFactory.generateUser();
-        delete body.confirmedPassword;
-        const token = jwt.sign(body.email, process.env.JWT_SECRET);
-        const response = await supertest(app).post("/sign-in").send(body);
-
-        expect(response.text).not.toBe(token);
-    });
 });
 
 describe("Post test router tests", () => {
